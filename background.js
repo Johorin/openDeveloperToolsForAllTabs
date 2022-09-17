c1b1181663f14c71efbc4825636a6cbf8688bc1e
北條佑弥 <y.hojo@core-tech.jp>
2022-09-17 23:19:02 +0900

    キーボードの入力は代行できてもコマンドは実行されないようになっててこの機能作るの無理でした

--- background.js
+++ background.js
@@ -5,30 +5,32 @@ let executeCommand = '';
 
 // Chrome拡張機能のアイコン押下で発火するイベント
 chrome.browserAction.onClicked.addListener(function () {
-    console.log('動いてるよ〜');
     // osによって実行するコマンド内容を変える
     if (ua.indexOf("mac os x") !== -1){
         // mac 開発者ツールショートカットコマンド設定（Command + option + i）
-        executeCommand = new KeyboardEvent('keydown', {
-            keyCode : 73,    // iキー
-            altKey  : true,  // optキー
+        executeCommand = new KeyboardEvent("keydown", {
+            keyCode : 82,    // rキー
+            // altKey  : true,  // optキー
             shiftKey: false, // shiftキー
             metaKey : true,  // Commandキー
-            key     : "i",   // iキー
+            key     : "r",   // iキー
         });
     } else if (ua.indexOf("windows nt") !== -1) {
         // windows 開発者ツールショートカットコマンド設定（F12）
-        executeCommand = new KeyboardEvent('keydown', {
+        executeCommand = new KeyboardEvent("keydown", {
             keyCode : 123,   // F12キー
             altKey  : false, // altキー
             shiftKey: false, // shiftキー
             key     : "F12", // F12キー
         });
     }
-    // if (request == "Action" && executeCommand != '') {
-    //     // executeCommandを実行
-    //     document.dispatchEvent(executeCommand);
-    // }
+    // return alert('拡張機能を実行しました');
+    if (executeCommand != '') {
+        // // executeCommandを実行
+        // document.dispatchEvent(executeCommand);
+        // console.log(executeCommand);
+        window.location.reload(true);
+    }
 });
 
 // // Chrome拡張機能のアイコン押下で発火するイベント
